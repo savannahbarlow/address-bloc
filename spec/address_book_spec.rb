28aac3ceb3cd9249c382f8abd9eaa05eba1d36ae
Savannah Barlow <barlow.savannah@gmail.com>
2016-04-02 22:02:35 -0700

    Checkpoint 20  menu: completed

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -1,18 +1,18 @@
 require_relative '../models/address_book'
 
 RSpec.describe AddressBook do
-    
+
     describe "attributes" do
         it "responds to entries" do
             book = AddressBook.new
             expect(book).to respond_to(:entries)
         end
-        
+
         it "initializes entries as an array" do
             book = AddressBook.new
             expect(book.entries).to be_an(Array)
         end
-        
+
         it "intializes entries as empty" do
             book = AddressBook.new
             expect(book.entries.size).to eq(0)
@@ -20,7 +20,7 @@ RSpec.describe AddressBook do
     end
 
     describe "#add_entry" do
-        
+
         it "adds only one entry to the address book" do
             book = AddressBook.new
             book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
@@ -31,10 +31,25 @@ RSpec.describe AddressBook do
             book = AddressBook.new
             book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
             new_entry = book.entries[0]
-    
+
             expect(new_entry.name).to eq('Ada Lovelace')
             expect(new_entry.phone_number).to eq('010.012.1815')
             expect(new_entry.email).to eq('augusta.king@lovelace.com')
         end
+    end
+
+    describe "#remove_entry" do
+      it "removes an entry using name, phone_number, and email" do
+        book = AddressBook.new
+
+        name = "Ada Lovelace"
+        phone_number = "010.012.1815"
+        email = "augusta.king@lovelace.com"
+        book.add_entry(name, phone_number, email)
+
+        expect(book.entries.size).to eq 1
+        book.remove_entry(name, phone_number, email)
+        expect(book.entries.size).to eq 0
+      end
     end
 end
